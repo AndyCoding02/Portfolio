a65e5abfde5ba2d8168c4776c212ba24e52aa50a
AndyCoding02 <drewazarov26@gmail.com>
2024-05-16 13:38:12 +0300

    uploaded

---
+++ script.js
@@ -0,0 +1,82 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Full-Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true
+})
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            sec.classList.add('show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+
+
+}
+
+
+function sendEmail() {
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+
+    emailjs.send("service_0oxm76e", "template_bzwkd6e", parms).then(
+        message => {
+            if (message == "OK")
+                console.aler(); {
+                Swal.fire({
+                    title: "Thank you for the time!",
+                    text: "I will get back to you shortly!",
+                    icon: "success"
+                });
+            }
+        },
+        error => {
+            console.error(error);
+            Swal.fire({
+                title: "Error!",
+                text: "Something went wrong!",
+                icon: "error"
+            });
+        }
+    );
+};
+
+
